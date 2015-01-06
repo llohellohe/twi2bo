85134f2dbcb9e047a19ed901d5deb7c82c496195
杨期(028611) <chris.yangq@alibaba-inc.com>
2015-01-06 16:42:55 +0800

    增加token

--- src/main/java/yangqi/sns/twi2bo/api/APIConstants.java
+++ src/main/java/yangqi/sns/twi2bo/api/APIConstants.java
@@ -10,7 +10,12 @@ public class APIConstants {
     public static final String CONSUMER_KEY="MvlI4hjlVdIzqvmeLDTkMwjC9";
     public static final String CONSUMER_SECRET="f3iBeNZVgqwT68y4lUNJydI8N7pGfzEMeTqoxtzB8hZIlk5ywo";
 
+    public static final String ACCESS_TOKEN="38597621-J7D8p9TOBhfRAFkz1jScL5ZZuy2mD8INXJkRSb2Aj";
+    public static final String ACCESS_TOKEN_SECRET="SOVJpCRS4BbguddvDA2raU2uJn93BtV2FLAZYWhkq7zh5";
+
     public static final String ENCODING="utf-8";
     public static final String CALLBACK_URL="http://www.hiyangqi.com";
+
+    public static final String HMAC_SHA1="HMAC-SHA1";
 
 }
