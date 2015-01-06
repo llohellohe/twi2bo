5482e2b20ca82cfdb9d995edddb21f9a7f0c4487
杨期(028611) <chris.yangq@alibaba-inc.com>
2015-01-06 16:44:25 +0800

    修改测试用例

--- src/test/java/yangqi/sns/twi2bo/api/APIUtilsTest.java
+++ src/test/java/yangqi/sns/twi2bo/api/APIUtilsTest.java
@@ -2,9 +2,6 @@ package yangqi.sns.twi2bo.api;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.Assert.assertEquals;
 
 public class APIUtilsTest {
--- src/test/java/yangqi/sns/twi2bo/api/sign/SignatureToolTest.java
+++ src/test/java/yangqi/sns/twi2bo/api/sign/SignatureToolTest.java
@@ -1,6 +1,7 @@
 package yangqi.sns.twi2bo.api.sign;
 
 import org.junit.Test;
+import yangqi.sns.twi2bo.api.APIParameter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -47,8 +48,24 @@ public class SignatureToolTest {
 
     @Test
     public void testSign(){
-        String sign=SignatureTool.sign("kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE","POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521");
+        String sign=SignatureTool.caculateSignature("kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE", "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521");
         assertEquals(sign,"tnnArxj06cWHq44gCs1OSKk/jLY=");
+    }
+
+    @Test
+    public void testCreateOAuthHeader(){
+
+        APIParameter parameter=new APIParameter();
+        parameter.setoAuthConsumerKey("xvz1evFS4wEEPTGEFPHBog");
+        parameter.setoAuthNonce("kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg");
+        parameter.setoAuthSignature("tnnArxj06cWHq44gCs1OSKk/jLY=");
+        parameter.setTimestamp("" + 1318622958);
+        parameter.setoAuthToken("370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb");
+        parameter.setoAuthVersion("1.0");
+
+        String oauthHeader=SignatureTool.createOAuthHeader(parameter);
+
+        assertEquals(oauthHeader,"OAuth oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\", oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\", oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1318622958\", oauth_token=\"370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb\", oauth_version=\"1.0\"");
     }
 
 }
